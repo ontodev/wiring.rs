a4615b4bab438006242a26f3f0aabc6c3a312257
chris <chris.kind.man@gmail.com>
2021-06-21 22:01:49 +0100

    Add translation for equivalent classes axioms

--- src/axiom_translation.rs
+++ src/axiom_translation.rs
@@ -1,5 +1,3 @@
-use serde_json::{Result};
-
 use crate::class_translation as class_translation; 
 
 //TODO: error handling (don't use unrwap() like that...)
@@ -11,6 +9,17 @@ pub fn translate_subclass_of_axiom(sub: &str, sup: &str) -> String {
     let lhs : String = class_translation::thick2ofn(&subclass);
     let rhs: String = class_translation::thick2ofn(&superclass); 
     let expression = format!("[\"SubClassOf\",{},{}]", lhs, rhs);
+    expression 
+}
+
+pub fn translate_equivalent_class(sub: &str, sup: &str) -> String {
+
+    let subclass: class_translation::OWL = serde_json::from_str(sub).unwrap(); 
+    let superclass: class_translation::OWL = serde_json::from_str(sup).unwrap(); 
+
+    let lhs : String = class_translation::thick2ofn(&subclass);
+    let rhs: String = class_translation::thick2ofn(&superclass); 
+    let expression = format!("[\"EquivalentClasses\",{},{}]", lhs, rhs);
     expression 
 }
 
