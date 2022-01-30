e9c2803b478912e747fe0606e00fdf71bad9ebf2
chris <chris.kind.man@gmail.com>
2022-01-30 15:18:01 +0000

    Simplify entry point for labelling translation

--- src/ofn_labeling/ofn_parser.rs
+++ src/ofn_labeling/ofn_parser.rs
@@ -2,33 +2,17 @@ use serde_json::{Value};
 use crate::ofn_labeling::axiom_translation as axiom_translation; 
 use std::collections::HashMap;
 
-//pub fn parse_ofn(t: &str, m : &HashMap<String,String>) -> String {
-//    //deserialise JSON as a (serde) Value
-//    let thick_triple: Value = serde_json::from_str(t).unwrap(); 
-//    //start the translation process
-//    let out = translate_triple(&thick_triple, m); 
-//    out 
-//}
+//TODO: there is no 'real' parsing going on
+//TODO: could refactor the whole labelling translation into a single file
+//since OFN-S expressions are handled in a uniform manner 
+pub fn parse_ofn(v: &Value, m : &HashMap<String,String>) -> Value { 
 
-pub fn parse_ofn(t: &Value, m : &HashMap<String,String>) -> Value {
-    //deserialise JSON as a (serde) Value
-    //let thick_triple: Value = serde_json::from_str(t).unwrap(); 
-    //start the translation process
-    let out = translate_triple(t, m); 
-    out 
+    match v[0].as_str() {
+        Some("SubClassOf") => axiom_translation::translate_subclass_of_axiom(v,m),
+        Some("DisjointClasses") => axiom_translation::translate_disjoint_classes_axiom(v,m),
+        Some("DisjointUnionOf") => axiom_translation::translate_disjoint_union_of_axiom(v,m),
+        Some("EquivalentClasses") => axiom_translation::translate_equivalent_classes_axiom(v,m),
+        Some(_) => panic!(),
+        None => Value::String(String::from(v.as_str().unwrap())),
+    }
 }
-
-pub fn translate_triple(v : &Value, m :&HashMap<String, String>) -> Value {
-
-    let owl_operator: String = v[0].to_string();
-
-     let res : Value = match owl_operator.as_str() {
-         "\"SubClassOf\"" => axiom_translation::translate_subclass_of_axiom(v,m),
-         "\"DisjointClasses\"" => axiom_translation::translate_disjoint_classes_axiom(v,m),
-         "\"DisjointUnionOf\"" => axiom_translation::translate_disjoint_union_of_axiom(v,m),
-         "\"EquivalentClasses\"" => axiom_translation::translate_equivalent_classes_axiom(v,m),
-         _ => Value::String(v.to_string()),
-     };
-
-     res
-} 
