1a67b02c654f5ba862c92dc0de26add54be48e02
chris <chris.kind.man@gmail.com>
2022-01-30 15:28:18 +0000

    Simplify entry point for type inference

--- src/ofn_typing/ofn_parser.rs
+++ src/ofn_typing/ofn_parser.rs
@@ -3,22 +3,13 @@ use crate::ofn_typing::axiom_translation as axiom_translation;
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-pub fn parse_ofn(t: &Value, m : &HashMap<String, HashSet<String>>) -> Value { 
-    //start the translation process
-    let out = translate_triple(&t, m); 
-    out 
-}
-
-pub fn translate_triple(v : &Value, m :&HashMap<String, HashSet<String>>) -> Value { 
-
-     let res : Value = match v[0].as_str() {
+pub fn parse_ofn(v: &Value, m : &HashMap<String, HashSet<String>>) -> Value { 
+     match v[0].as_str() {
          Some("SubClassOf") => axiom_translation::translate_subclass_of_axiom(v,m),
          Some("DisjointClasses") => axiom_translation::translate_disjoint_classes_axiom(v,m),
          Some("DisjointUnionOf") => axiom_translation::translate_disjoint_union_of_axiom(v,m),
          Some("EquivalentClasses") => axiom_translation::translate_equivalent_classes_axiom(v,m),
          Some(_) => panic!(),
          None => Value::String(String::from(v.as_str().unwrap())),
-     };
-
-     res
+     } 
 } 
