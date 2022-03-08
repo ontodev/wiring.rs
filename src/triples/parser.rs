caf40a82f15e05faa7b7e9c11512fa704a4625f9
chris <chris.kind.man@gmail.com>
2022-03-08 13:33:25 +0000

    Add thin triple extraction from a file

--- src/triples/parser.rs
+++ src/triples/parser.rs
@@ -23,6 +23,34 @@ pub fn extract_class_expression_axioms(path : &str) -> Vec<Value> {
     axioms 
 }
 
+pub fn extract_thin_triples(path : &str) -> Vec<Value> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut triples = Vec::new();
+
+    for line in reader.lines() {
+        let content : String = line.unwrap(); 
+        let thick_triple: Value = serde_json::from_str(content.as_str()).unwrap();
+
+        if !is_thick_triple(&thick_triple){//TODO: test that all thee thinsg are strings
+            triples.push(thick_triple);
+        }
+    }
+    triples 
+}
+
+fn is_thick_triple(v: &Value) -> bool {
+
+    let nesting = v["subject"].is_object() | v["predicate"].is_object() | v["object"].is_object();
+    if nesting  {
+        true
+    } else {
+        v["predicate"].as_str().unwrap().eq("rdfs:subClassOf")
+    } 
+}
+
+
 fn is_class_expression_axiom(v: &Value) -> bool {
 
     let predicate = v["predicate"].as_str();
