92cf0dd244c7c751c6f1cac85cfb0736dbec81f4
chris <chris.kind.man@gmail.com>
2022-03-08 13:08:27 +0000

    Use as_str() instead of to_string() for serde:Value strings

--- src/ofn_labeling/property_translation.rs
+++ src/ofn_labeling/property_translation.rs
@@ -3,17 +3,16 @@ use std::collections::HashMap;
 
 pub fn translate(v : &Value, m : &HashMap<String, String>) -> Value {
 
-    let owl_operator: String = v[0].to_string();
-
-     match owl_operator.as_str() {
-         "\"ObjectInverseOf\"" => translate_inverse_of(v,m),
-         _ => Value::String(v.to_string())
-     }
+     match v[0].as_str() {
+         Some("ObjectInverseOf") => translate_inverse_of(v,m), 
+         Some(_) => panic!(),
+         None => Value::String(String::from(v.as_str().unwrap())),
+     } 
 }
 
 pub fn translate_inverse_of(v : &Value, m : &HashMap<String, String>) -> Value {
 
-    let operator: Value = v[0].clone();
+    let operator = Value::String(String::from("ObjectInverseOf"));
     let argument: Value = translate(&v[1],m);
 
     let v = vec![operator, argument];
