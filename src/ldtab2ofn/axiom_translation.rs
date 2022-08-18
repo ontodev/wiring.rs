21eced7107b91ac3d3fafedee14c6c594bf5f1a4
chris <chris.kind.man@gmail.com>
2022-08-18 23:41:11 +0100

    Add translation for declarations

--- src/ldtab2ofn/axiom_translation.rs
+++ src/ldtab2ofn/axiom_translation.rs
@@ -69,6 +69,61 @@ pub fn translate_disjoint_union(union: &owl::OWL, operands: &owl::OWL) -> Value 
     Value::Array(union.to_vec())
 }
 
+pub fn get_ofn_operator(op : &str) -> Value {
+
+    match op {
+        "rdfs:Datatype" => Value::String(String::from("Datatype")),
+        "owl:Class" => Value::String(String::from("Class")),
+        "owl:ObjectProperty" => Value::String(String::from("ObjectProperty")),
+        "owl:DatatypeProperty" => Value::String(String::from("DataProperty")),
+        "owl:AnnotationProperty" => Value::String(String::from("AnnotationProperty")),
+        "owl:NamedIndividual" => Value::String(String::from("NamedIndividual")),
+
+        //TODO ambiguous
+        "owl:FunctionalProperty" => Value::String(String::from("FunctionalProperty")),
+
+        "owl:InverseFunctionalProperty" => Value::String(String::from("InverseObjectFunctionalProperty")),
+        "owl:ReflexiveProperty" => Value::String(String::from("ReflexiveObjectProperty")),
+        "owl:IrreflexiveProperty" => Value::String(String::from("IrreflexiveObjectProperty")),
+        "owl:SymmetricProperty" => Value::String(String::from("SymmetricObjectProperty")),
+        "owl:AsymmetricProperty" => Value::String(String::from("AsymmetricObjectProperty")),
+        "owl:TransitiveProperty" => Value::String(String::from("TransitiveObjectProperty")),
+
+        _ => Value::String(String::from("ClassAssertion")), 
+    } 
+} 
+
+pub fn translate_rdf_type(lhs: &owl::OWL, rhs: &owl::OWL) -> Value {
+
+
+    let operator = match rhs {
+        owl::OWL::Named(x) => get_ofn_operator(x),
+        _ => panic!(),//TODO: 
+    };
+
+    let lhs : Value = class_translation::translate(lhs);
+    let rhs: Value = class_translation::translate(rhs); 
+
+    //check whether operator is a declaration
+    if operator.to_string().eq("\"Datatype\"") ||
+       operator.to_string().eq("\"Class\"") ||
+       operator.to_string().eq("\"ObjectProperty\"") ||
+       operator.to_string().eq("\"DataProperty\"") ||
+       operator.to_string().eq("\"AnnotationProperty\"") ||
+       operator.to_string().eq("\"NamedIndividual\"")
+    {
+        let v = vec![operator, lhs, rhs];
+        let v = Value::Array(v);
+
+        let v = vec![Value::String(String::from("Declaration")),v];
+        Value::Array(v)
+
+    } else { 
+        let v = vec![operator, lhs, rhs];
+        Value::Array(v) 
+    }
+}
+
 pub fn translate_thin_triple(s: &str, p: &str, o: &str) -> Value {
 
     let subject = Value::String(String::from(s));
