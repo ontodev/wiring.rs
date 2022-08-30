e029a0acb356cf8e51b841c04f58ef288cd8c614
chris <chris.kind.man@gmail.com>
2022-08-30 21:44:42 +0100

    Fix encoding of annotations

--- src/ldtab2ofn/annotation_translation.rs
+++ src/ldtab2ofn/annotation_translation.rs
@@ -1,6 +1,36 @@
 use serde_json::{Value, Result as SResult, Map};
 use serde_json::json;
 
+pub fn translate_annotations(m : &Map<String, Value>) -> Vec<Value> {
+
+    let mut annotations = Vec::new();
+
+    for (k, v) in m {
+
+        let property = Value::String(String::from(k)); 
+
+        match v {
+            //traverse list of objects
+            Value::Array(array) =>  {
+                for object in array { 
+                    //unpack object
+                    let value = object["object"].clone(); 
+                    //TODO:
+                    //(horned OWL currently doesn't support recrusive annotations)
+                    //1. check whether object has an annotation
+                    //2. translate annotation recursively
+                    //3. insert annotation at the second index
+                    let annotation_operator = Value::String(String::from("Annotation")); 
+                    let annotation = vec![annotation_operator, property.clone(), value];
+                    annotations.push(Value::Array(annotation));
+                } 
+            } 
+            _ => panic!("Not an array"), 
+        }; 
+    }
+    annotations 
+}
+
 pub fn translate_annotation(m : &Map<String, Value>) -> Value {
 
     let operator = Value::String(String::from("Annotation")); 
@@ -49,6 +79,7 @@ pub fn translate_annotation_list(m : &Map<String, Value>) -> Value {
             Value::Array(array) =>  {
                 for object in array { 
                     //unpack object
+                    //TODO: use datatype key to encode literals correctly
                     let value = object["object"].clone(); 
                     //TODO:
                     //(horned OWL currently doesn't support recrusive annotations)
@@ -87,16 +118,17 @@ pub fn is_single_annotation(m : &Map<String, Value>) -> bool {
 }
 
 
-pub fn translate(annotation : &Value) -> Value { 
+pub fn translate(annotation : &Value) -> Vec<Value> { 
      match annotation {
-        Value::Object(x) => {
-            if is_single_annotation(&x) {
-                translate_annotation(&x)
-            } else {
-                translate_annotation_list(&x)
-            }
-        },
-        Value::String(x) => annotation.clone(),
+        Value::Object(x) => translate_annotations(&x),
+        //{
+        //    if is_single_annotation(&x) {
+        //        translate_annotation(&x)
+        //    } else {
+        //        translate_annotation_list(&x)
+        //    }
+        //},
+        //Value::String(x) => annotation.clone(),
         _ => panic!("Not a valid annotation map"), 
     } 
 }
