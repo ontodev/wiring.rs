e23b9ba474bb580b2bc10fcd500404a928dff189
chris <chris.kind.man@gmail.com>
2022-10-02 16:11:22 +0100

    Update module dependencies in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 pub mod thick2ofn;
 pub mod ofn2thick;
 pub mod ofn2man;
@@ -5,34 +7,140 @@ pub mod owl;
 pub mod ofn_labeling;
 pub mod ofn_typing;
 pub mod triples;
+pub mod ofn_util;
+pub mod ofn_2_rdfa;
+pub mod ldtab2ofn;
+pub mod ofn2ldtab;
+use std::collections::HashMap;
+use serde_json::json; 
 
-use serde_json::{Value};
 
-fn main() {
+fn main(){
+    let path = String::from("resources/thickTest.txt"); 
+
+    //let entity_2_label = ofn_labeling::labeling::extract_labeling(&path);
+    let entity_2_type = ofn_typing::typing::extract_typing(&path); 
+    let triples = triples::parser::get_thick_triples(&path);
+
+    for triple in triples.iter() { 
+         println!("Triple: {}", triple );//thick triples are given
+
+         let ofn = ldtab2ofn::thick_triple_parser::parse_ldtab(triple.to_string().as_str());
+         println!("OFN S: {}", ofn );//transformation to OFN S-expression 
+
+         let ofn_typed  = ofn_typing::ofn_parser::parse_ofn(&ofn, &entity_2_type);
+         println!("Typed: {}", ofn_typed);//OFN S-expression after typing
+
+         println!("");
+    }
+
+}
+
+fn playground() {
 
     //example file with thick triples
     let path = String::from("resources/thickOBI.txt"); 
+    //let path = String::from("resources/testOBI.txt"); 
 
     //get thick triples that encode class expression axioms
     let axioms = triples::parser::extract_class_expression_axioms(&path);
+    //let thin_triples = triples::parser::extract_thin_triples(&path);
 
     //get OWL type information and entity labels
     let entity_2_label = ofn_labeling::labeling::extract_labeling(&path);
     let entity_2_type = ofn_typing::typing::extract_typing(&path); 
 
+    //println!("Label: {:?}", entity_2_label);
+    //let target = String::from("\"obo:OBI_2100162\"");
+    //println!("Label: {:?}", entity_2_label.get(&target));
+    //let t : &str = entity_2_label.get(&target).unwrap().as_str();
+    //let size = t.len();
+    //let tt = &t[1..size-1];
+    //println!("Label: {:?}", tt);
+
+    let mut prefix_2_expansion = HashMap::new();
+    prefix_2_expansion.insert("obo".to_string(),"OBO#".to_string());
+    //prefix_2_expansion.insert("obo".to_string(),"<http://purl.obolibrary.org/obo/>".to_string());
+
+    let mut iri_2_prefix = HashMap::new();
+    iri_2_prefix.insert("<OBO#>".to_string(),"obo:".to_string());
+    //iri_2_prefix.insert("<http://purl.obolibrary.org/obo/>".to_string(),"obo:".to_string());
+
+    //for triple in thin_triples.iter(){
+    //     println!("Triple: {}", triple );//thick triples are given
+    //     let ofn = thick2ofn::thick_triple_parser::parse_triple(triple.to_string().as_str());
+    //     println!("OFN S: {}", ofn );//transformation to OFN S-expression
+    //     let ofn_typed  = ofn_typing::ofn_parser::parse_ofn(&ofn, &entity_2_type);
+    //     println!("Typed: {}", ofn_typed);//OFN S-expression after typing
+    //     let ofn_labelled = ofn_labeling::ofn_parser::parse_ofn(&ofn_typed, &entity_2_label);
+    //     println!("Labelled: {}", ofn_labelled);//OFN S-expression after labelling 
+    //     println!(""); 
+    //}
+
+    let ex = ofn2ldtab::util::translate_datatype(&json!("\"asd\""));
+    println!("AA {}", ex.as_str().unwrap());
+
+
     for triple in axioms.iter() { 
-         println!("Triple: {}", triple );
+         println!("Triple: {}", triple );//thick triples are given
+
+         let triple_sorted = ofn2ldtab::util::sort_value(&triple); 
+         println!("Triple Sorted: {}", triple_sorted);//transformation to OFN S-expression 
+
          let ofn = thick2ofn::thick_triple_parser::parse_triple(triple.to_string().as_str());
-         println!("OFN-S: {}", ofn );
+         println!("OFN S: {}", ofn );//transformation to OFN S-expression 
+
+         let sig = ofn_util::signature::extract_identifiers(&ofn);
+         println!("Signature: {:?}", sig );//transformation to OFN S-expression 
+
+
+         let ofn_sorted = ofn2ldtab::util::sort_value(&ofn); 
+         println!("OFN Sorted: {}", ofn_sorted );//transformation to OFN S-expression 
+
+
+         let tt = ofn2ldtab::ofn_parser::translate_triple(&ofn);
+         println!("ThickTriple: {}", tt );//transformation to OFN S-expression 
+
+         //let t = ofn2RDFa::ofn_parser::translate(&ofn);
+         //println!("Type: {}", t);//transformation to OFN S-expression
          let ofn_typed  = ofn_typing::ofn_parser::parse_ofn(&ofn, &entity_2_type);
-         println!("Typed: {}", ofn_typed);
+         println!("Typed: {}", ofn_typed);//OFN S-expression after typing
+
+
          let ofn_labelled = ofn_labeling::ofn_parser::parse_ofn(&ofn_typed, &entity_2_label);
-         println!("Labelled: {}", ofn_labelled);
+         let ofn_labelled_untyped = ofn_labeling::ofn_parser::parse_ofn(&ofn, &entity_2_label);
+
+         println!("Labelled: {}", ofn_labelled);//OFN S-expression after labelling
+         println!("Untyped Labelled: {}", ofn_labelled_untyped);//OFN S-expression after labelling
+
          let man_str = ofn2man::parser::translate_triple(&ofn_typed);
          let man_str_labelled = ofn2man::parser::translate_triple(&ofn_labelled);
+
+         let man_str_untyped = ofn2man::parser::translate_triple(&ofn);
+
+         println!("Manchester (untyped): {}", man_str_untyped); 
          println!("Manchester: {}", man_str); 
          println!("Lanchester: {}", man_str_labelled); 
+
+         let thick_triple = ofn2thick::ofn_parser::translate_triple(&ofn_typed);
+         println!("Thick: {}", thick_triple); 
+         let signature = ofn_util::signature::extract(&ofn_typed);
+         println!("Signature: {:?}", signature); 
+         let prefixes = ofn_util::signature::get_prefixes(&ofn_typed);
+         println!("Prefixes: {:?}", prefixes); 
+         let iri = ofn_util::curie_to_iri::translate(&ofn_typed,&prefix_2_expansion);
+         println!("Test Curie2IRI: {}", iri); 
+         let curie = ofn_util::iri_2_curie::translate(&iri,&iri_2_prefix);
+         println!("Test IRI2Curie: {}", curie); 
+         //let iri = ofn_util::structural_identity::translate(&ofn_typed,&prefix_2_expansion);
+         let ofn_rdfa = ofn_2_rdfa::ofn_parser::translate(&ofn_typed, &entity_2_label);
+         println!("Test RDFa: {}", ofn_rdfa); 
+         //println!("Object: {}", triple["object"]);
+         //let object = triple["object"].to_string();
+         //println!("Object: {}", object.as_str());//TODO this stuff doesn't have datatypes
+         //let typed_object =  ldtab2ofn::thick_triple_parser::parse_thick_triple_object(object.as_str());
+         //let object_ofn =  ldtab2ofn::class_translation::translate(&typed_object);
+         //println!("Object OFN: {}", object_ofn);
          println!("");
     }
 }
-
