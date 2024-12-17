a27ee0554f1bfd188c1d34d79afea498de7c537b
ckindermann <chris.kind.man@gmail.com>
2024-12-16 17:01:51 -0800

    Formatting

--- src/ofn_2_ldtab/annotation_translation.rs
+++ src/ofn_2_ldtab/annotation_translation.rs
@@ -1,60 +1,57 @@
-use serde_json::{Value};
-use serde_json::json; 
+use regex::Regex;
+use serde_json::json;
+use serde_json::Value;
 use std::collections::HashMap;
-use regex::Regex; 
 
-pub fn is_annotation(v : &Value) -> bool { 
-    match v.clone() { 
-        Value::Array(x) => { 
-            match x[0].as_str(){
+pub fn is_annotation(v: &Value) -> bool {
+    match v.clone() {
+        Value::Array(x) => {
+            match x[0].as_str() {
                 Some("Annotation") => true,
                 //Some("AnnotationList") => true, //NB: this shouldn't occur
                 Some(_) => false,
-                None => false, 
+                None => false,
             }
         }
         _ => false,
-    } 
+    }
 }
 
-pub fn has_annotation(v : &Value) -> bool { 
+pub fn has_annotation(v: &Value) -> bool {
     match v.clone() {
         Value::Array(x) => is_annotation(&x[1]), //look into second argument
         _ => false,
-    } 
+    }
 }
 
-pub fn strip_annotations(v : &Value) -> Value {
-
+pub fn strip_annotations(v: &Value) -> Value {
     let mut res = Vec::new();
     let original = &v.as_array().unwrap()[0..];
-    for element in original { 
-        if !is_annotation(element){
+    for element in original {
+        if !is_annotation(element) {
             res.push(element.clone());
-        } 
-    } 
-    Value::Array(res) 
+        }
+    }
+    Value::Array(res)
 }
 
-pub fn get_owl(v : &Value) -> Value { 
+pub fn get_owl(v: &Value) -> Value {
     strip_annotations(v)
 }
 
-
-pub fn get_annotations(v : &Value) -> Vec<Value> {
+pub fn get_annotations(v: &Value) -> Vec<Value> {
     if has_annotation(&v) {
-
         let mut res = Vec::new();
         let candidates = &v.as_array().unwrap()[0..];
-        for candidate in candidates  {
-            if is_annotation(candidate){
+        for candidate in candidates {
+            if is_annotation(candidate) {
                 res.push(candidate.clone());
-            } 
+            }
         }
         res
     } else {
-        Vec::new()//empty vector
-    } 
+        Vec::new() //empty vector
+    }
 }
 
 pub fn is_literal(value: &Value) -> bool {
@@ -68,60 +65,63 @@ pub fn is_literal(value: &Value) -> bool {
         let iri_or_curie_re = Regex::new("^\"(?s)(.*)\"\\^\\^(.*)$").unwrap();
 
         // Check if the string matches any of the forms
-        return simple_string_re.is_match(s) || lang_tag_re.is_match(s) || iri_or_curie_re.is_match(s);
+        return simple_string_re.is_match(s)
+            || lang_tag_re.is_match(s)
+            || iri_or_curie_re.is_match(s);
     }
     false
 }
 
 pub fn translate_literal(s: &str) -> Value {
-
     let language_tag = Regex::new("^\"(?s)(.*)\"@(.*)$").unwrap();
     let datatype = Regex::new("^\"(?s)(.*)\"\\^\\^(.*)$").unwrap();
     let plain = Regex::new("^\"(?s)(.*)\"$").unwrap();
 
     if language_tag.is_match(s) {
-        match language_tag.captures(s){
-            Some(x) => { let lang = format!("@{}", &x[2]);
+        match language_tag.captures(s) {
+            Some(x) => {
+                let lang = format!("@{}", &x[2]);
                 json!({"object" : &x[1],
                        "meta" : "owl:Axiom",
-                       "datatype" : lang}) 
-            }, 
-            None => json!("Error"), 
-        } 
+                       "datatype" : lang})
+            }
+            None => json!("Error"),
+        }
     } else if datatype.is_match(s) {
-        match datatype.captures(s){
-            Some(x) => { let data = format!("{}", &x[2]); 
+        match datatype.captures(s) {
+            Some(x) => {
+                let data = format!("{}", &x[2]);
                 json!({"object" : &x[1],
                        "meta" : "owl:Axiom",
-                        "datatype" : data})},
-            None => json!("Error"), 
-        } 
+                        "datatype" : data})
+            }
+            None => json!("Error"),
+        }
     } else if plain.is_match(s) {
-        match plain.captures(s){
-            Some(x) => { 
+        match plain.captures(s) {
+            Some(x) => {
                 json!({"object" : &x[1],
                        "meta" : "owl:Axiom",
-                        "datatype" : "xsd:string"})},
-            None => json!("Error"), 
-        } 
-    
-    }  else {
+                        "datatype" : "xsd:string"})
+            }
+            None => json!("Error"),
+        }
+    } else {
         json!("error")
         //json!({"object" : s, "datatype": "_plain"})
     }
 }
 
-pub fn translate_value(v : &Value) -> Value { 
+pub fn translate_value(v: &Value) -> Value {
+    let s = v.as_str().unwrap();
 
-    let s = v.as_str().unwrap(); 
-
-    let literal = Regex::new("^\"(?s)(.+)\"(.*)$").unwrap(); 
-    let uri = Regex::new("^<(.+)>$").unwrap(); 
+    let literal = Regex::new("^\"(?s)(.+)\"(.*)$").unwrap();
+    let uri = Regex::new("^<(.+)>$").unwrap();
     let curie = Regex::new("^(.+):(.+)$").unwrap();
 
     if literal.is_match(s) {
         translate_literal(s)
-    } else if uri.is_match(s) { 
+    } else if uri.is_match(s) {
         json!({"object" : s,
                "meta" : "owl:Axiom",
                "datatype" : "_IRI"})
@@ -131,26 +131,25 @@ pub fn translate_value(v : &Value) -> Value {
                "datatype" : "_IRI"})
     } else {
         json!("ERROR")
-    } 
+    }
 }
 
-pub fn translate_annotations(annotations : &Vec<Value>) -> Value {
-
-    let mut property_2_value = HashMap::new(); 
+pub fn translate_annotations(annotations: &Vec<Value>) -> Value {
+    let mut property_2_value = HashMap::new();
     for annotation in annotations {
         let a = annotation.as_array().unwrap();
         let property = a[1].clone();
         let property_str = String::from(property.as_str().unwrap());
-        let value = translate_value(&a[2].clone()); 
+        let value = translate_value(&a[2].clone());
 
-        //collect all annotations with the same property 
+        //collect all annotations with the same property
         if !property_2_value.contains_key(&property_str) {
-            let mut vec = Vec::new(); 
-            vec.push(value); 
-            property_2_value.insert(property_str,vec);
+            let mut vec = Vec::new();
+            vec.push(value);
+            property_2_value.insert(property_str, vec);
         } else {
-            property_2_value.get_mut(&property_str).unwrap().push(value); 
-        } 
-    } 
-    json!(property_2_value) 
+            property_2_value.get_mut(&property_str).unwrap().push(value);
+        }
+    }
+    json!(property_2_value)
 }
