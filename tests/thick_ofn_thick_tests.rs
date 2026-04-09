2922db4c7b564080600513f0e06d5b0242c589e1
ckindermann <chris.kind.man@gmail.com>
2026-04-08 23:41:24 -0700

    Expand IRIs in tests

--- tests/thick_ofn_thick_tests.rs
+++ tests/thick_ofn_thick_tests.rs
@@ -32,7 +32,7 @@ fn round_trip(input: &str) -> bool {
 #[test]
 fn sub_class_of_axiom() {
     let subclass_of = r#" {"subject": "ex:subClass",
-                           "predicate": "rdfs:subClassOf",
+                           "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
                            "object": "ex:superClass",
                            "datatype" : "_IRI",
                            "annotation" : {},
@@ -44,9 +44,9 @@ fn sub_class_of_axiom() {
 
 #[test]
 fn test_n_ary_equivalence_axiom() {
-    let equivalence = r#" {"subject": "<ldtab:blanknode:e290d01aaeeb3aa3b9960e2b9299214d83ce6f224c3da50084915d473595dc75>",
-                           "predicate": "owl:equivalentClass",
-                           "object": {"owl:members" : [{"object" : [
+    let equivalence = r#" {"subject": "<ldtab:blanknode:f7107cdca26f46c56022cdecca9f2eb6f0538da8a81ae09f419801110a4bd694>",
+                           "predicate": "<http://www.w3.org/2002/07/owl#equivalentClass>",
+                           "object": {"<http://www.w3.org/2002/07/owl#members>" : [{"object" : [
                                                        {"object": "ex:equivalent1", "datatype" : "_IRI"},
                                                        {"object": "ex:equivalent2", "datatype" : "_IRI"},
                                                        {"object": "ex:equivalent3", "datatype" : "_IRI"}],
@@ -64,7 +64,7 @@ fn test_n_ary_equivalence_axiom() {
 fn binary_equivalence_axiom() {
     let equivalence =
         r#" {"subject": "ex:lhs",
-             "predicate": "owl:equivalentClass",
+             "predicate": "<http://www.w3.org/2002/07/owl#equivalentClass>",
              "object": "ex:rhs",
              "datatype" : "_IRI",
              "annotation" : {},
@@ -77,7 +77,7 @@ fn binary_equivalence_axiom() {
 #[test]
 fn disjoint_union_axiom() {
     let disjoint_union = r#" {"subject": "ex:disjointUnion",
-                              "predicate": "owl:disjointUnionOf",
+                              "predicate": "<http://www.w3.org/2002/07/owl#disjointUnionOf>",
                               "object": [{"object": "ex:u1", "datatype" : "_IRI"},
                                          {"object": "ex:u2", "datatype" : "_IRI"},
                                          {"object": "ex:u3", "datatype" : "_IRI"}],
@@ -92,9 +92,9 @@ fn disjoint_union_axiom() {
 #[test]
 fn complement_of_expression() {
     let complement_of = r#" {"subject": "ex:complementOf",
-                             "predicate": "owl:equivalentClass",
-                             "object": {"rdf:type": [{"object": "owl:Class", "datatype": "_IRI"}],
-                                        "owl:complementOf": [{"object": "ex:complement", "datatype": "_IRI"}]}, 
+                             "predicate": "<http://www.w3.org/2002/07/owl#equivalentClass>",
+                             "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Class>", "datatype": "_IRI"}],
+                                        "<http://www.w3.org/2002/07/owl#complementOf>": [{"object": "ex:complement", "datatype": "_IRI"}]}, 
                            "datatype" : "_JSONMAP",
                            "annotation" : {},
                            "graph" : "graph",
@@ -106,10 +106,10 @@ fn complement_of_expression() {
 #[test]
 fn some_values_from() {
     let existential_restriction = r#" {"subject": "ex:existential",
-                                       "predicate": "rdfs:subClassOf",
-                                       "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                                  "owl:onProperty": [{"object": "ex:pExistential", "datatype": "_IRI"}],
-                                                  "owl:someValuesFrom": [{"object": "ex:existentialFiller", "datatype": "_IRI"}]},
+                                       "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                                       "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                                  "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pExistential", "datatype": "_IRI"}],
+                                                  "<http://www.w3.org/2002/07/owl#someValuesFrom>": [{"object": "ex:existentialFiller", "datatype": "_IRI"}]},
                                         "datatype" : "_JSONMAP",
                                         "annotation" : {},
                                         "graph" : "graph",
@@ -121,10 +121,10 @@ fn some_values_from() {
 #[test]
 fn has_self() {
     let has_self = r#" {"subject": "ex:hasSelf",
-                        "predicate": "rdfs:subClassOf",
-                        "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                   "owl:onProperty": [{"object": "ex:pHasSelf", "datatype": "_IRI"}],
-                                   "owl:hasSelf": [{"object": "true^^xsd:boolean", "datatype": "_IRI"}]},
+                        "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                        "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                   "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pHasSelf", "datatype": "_IRI"}],
+                                   "<http://www.w3.org/2002/07/owl#hasSelf>": [{"object": "true", "datatype": "<http://www.w3.org/2001/XMLSchema#boolean>"}]},
                         "datatype" : "_JSONMAP",
                         "annotation" : {},
                         "graph" : "graph",
@@ -136,10 +136,10 @@ fn has_self() {
 #[test]
 fn has_value() {
     let has_value = r#" {"subject": "ex:hasValue",
-                         "predicate": "rdfs:subClassOf",
-                         "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                    "owl:onProperty": [{"object": "ex:pHasValue", "datatype": "_IRI"}],
-                                    "owl:hasValue": [{"object": "ex:a1", "datatype": "_IRI"}]},
+                         "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                         "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                    "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pHasValue", "datatype": "_IRI"}],
+                                    "<http://www.w3.org/2002/07/owl#hasValue>": [{"object": "ex:a1", "datatype": "_IRI"}]},
                          "datatype" : "_JSONMAP",
                          "annotation" : {},
                          "graph" : "graph",
@@ -151,10 +151,10 @@ fn has_value() {
 #[test]
 fn max_cardinality() {
     let max_cardinality = r#" {"subject": "ex:maxCardinality",
-                               "predicate": "rdfs:subClassOf",
-                               "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                          "owl:onProperty": [{"object": "ex:pMaxCardinality", "datatype": "_IRI"}],
-                                          "owl:maxCardinality": [{"object": "1", "datatype": "xsd:nonNegativeInteger"}]},
+                               "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                               "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                          "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pMaxCardinality", "datatype": "_IRI"}],
+                                          "<http://www.w3.org/2002/07/owl#maxCardinality>": [{"object": "1", "datatype": "<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>"}]},
                                 "datatype" : "_JSONMAP",
                                 "annotation" : {},
                                 "graph" : "graph",
@@ -166,11 +166,11 @@ fn max_cardinality() {
 #[test]
 fn max_qualified_cardinality() {
     let max_qualified_cardinality = r#" {"subject": "ex:maxQualifiedCardinality",
-                                         "predicate": "rdfs:subClassOf",
-                                         "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                                    "owl:onProperty": [{"object": "ex:pMaxQualifiedCardinality", "datatype": "_IRI"}],
-                                                    "owl:maxQualifiedCardinality": [{"object": "1", "datatype": "xsd:nonNegativeInteger"}],
-                                                    "owl:onClass": [{"object": "ex:maxCardinalityFiller", "datatype": "_IRI"}]},
+                                         "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                                         "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                                    "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pMaxQualifiedCardinality", "datatype": "_IRI"}],
+                                                    "<http://www.w3.org/2002/07/owl#maxQualifiedCardinality>": [{"object": "1", "datatype": "<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>"}],
+                                                    "<http://www.w3.org/2002/07/owl#onClass>": [{"object": "ex:maxCardinalityFiller", "datatype": "_IRI"}]},
 
                                         "datatype" : "_JSONMAP",
                                         "annotation" : {},
@@ -183,10 +183,10 @@ fn max_qualified_cardinality() {
 #[test]
 fn min_cardinality() {
     let min_cardinality = r#" {"subject": "ex:minCardinality",
-                               "predicate": "owl:equivalentClass",
-                               "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                               "owl:onProperty": [{"object": "ex:pMinCardinality", "datatype": "_IRI"}],
-                               "owl:minCardinality": [{"object": "1", "datatype" : "xsd:nonNegativeInteger"}]},
+                               "predicate": "<http://www.w3.org/2002/07/owl#equivalentClass>",
+                               "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                               "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pMinCardinality", "datatype": "_IRI"}],
+                               "<http://www.w3.org/2002/07/owl#minCardinality>": [{"object": "1", "datatype" : "<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>"}]},
                                "datatype" : "_JSONMAP",
                                "annotation" : {},
                                "graph" : "graph",
@@ -198,11 +198,11 @@ fn min_cardinality() {
 #[test]
 fn min_qualified_cardinality() {
     let min_qualified_cardinality = r#" {"subject": "ex:minQualifiedCardinality",
-                                         "predicate": "rdfs:subClassOf",
-                                         "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                                    "owl:onProperty": [{"object": "ex:pMinQualifiedCardinality", "datatype": "_IRI"}],
-                                                    "owl:minQualifiedCardinality": [{"object": "1", "datatype" : "xsd:nonNegativeInteger"}],
-                                                    "owl:onClass": [{"object": "ex:minCardinalityFiller", "datatype": "_IRI"}]},
+                                         "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                                         "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                                    "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pMinQualifiedCardinality", "datatype": "_IRI"}],
+                                                    "<http://www.w3.org/2002/07/owl#minQualifiedCardinality>": [{"object": "1", "datatype" : "<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>"}],
+                                                    "<http://www.w3.org/2002/07/owl#onClass>": [{"object": "ex:minCardinalityFiller", "datatype": "_IRI"}]},
                                "datatype" : "_JSONMAP",
                                "annotation" : {},
                                "graph" : "graph",
@@ -215,10 +215,10 @@ fn min_qualified_cardinality() {
 #[test]
 fn exact_cardinality_expression() {
     let exact_cardinality = r#" {"subject": "ex:exactCardinality",
-                                 "predicate": "rdfs:subClassOf",
-                                 "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                            "owl:onProperty": [{"object": "ex:pExactCardinality", "datatype": "_IRI"}],
-                                            "owl:cardinality": [{"object": "2", "datatype" : "xsd:nonNegativeInteger"}]},
+                                 "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                                 "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                            "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pExactCardinality", "datatype": "_IRI"}],
+                                            "<http://www.w3.org/2002/07/owl#cardinality>": [{"object": "2", "datatype" : "<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>"}]},
                                "datatype" : "_JSONMAP",
                                "annotation" : {},
                                "graph" : "graph",
@@ -231,11 +231,11 @@ fn exact_cardinality_expression() {
 #[test]
 fn exact_qualified_cardinality_expression() {
     let exact_qualified_cardinality = r#" {"subject": "ex:exactQualifiedCardinality",
-                                           "predicate": "rdfs:subClassOf",
-                                           "object": {"rdf:type": [{"object": "owl:Restriction", "datatype": "_IRI"}],
-                                                      "owl:onProperty": [{"object": "ex:pExactQualifiedCardinality", "datatype": "_IRI"}],
-                                                      "owl:qualifiedCardinality": [{"object": "2", "datatype" : "xsd:nonNegativeInteger"}],
-                                                      "owl:onClass": [{"object": "ex:exactQualifiedCardinalityFiller", "datatype" : "_IRI"}]},
+                                           "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                                           "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Restriction>", "datatype": "_IRI"}],
+                                                      "<http://www.w3.org/2002/07/owl#onProperty>": [{"object": "ex:pExactQualifiedCardinality", "datatype": "_IRI"}],
+                                                      "<http://www.w3.org/2002/07/owl#qualifiedCardinality>": [{"object": "2", "datatype" : "<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>"}],
+                                                      "<http://www.w3.org/2002/07/owl#onClass>": [{"object": "ex:exactQualifiedCardinalityFiller", "datatype" : "_IRI"}]},
                                "datatype" : "_JSONMAP",
                                "annotation" : {},
                                "graph" : "graph",
@@ -248,12 +248,12 @@ fn exact_qualified_cardinality_expression() {
 #[test]
 fn intersection_expression() {
     let intersection = r#" {"subject": "ex:intersection",
-                            "predicate": "rdfs:subClassOf",
-                            "object": {"owl:intersectionOf": [{"object" :  [{"object" : "ex:I1", "datatype": "_IRI"},
+                            "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                            "object": {"<http://www.w3.org/2002/07/owl#intersectionOf>": [{"object" :  [{"object" : "ex:I1", "datatype": "_IRI"},
                                                                             {"object" : "ex:I2", "datatype": "_IRI"},
                                                                             {"object" : "ex:I3", "datatype": "_IRI"}],
                                                               "datatype": "_JSONLIST"}],
-                                      "rdf:type": [{"object": "owl:Class", "datatype" : "_IRI"}]},
+                                      "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Class>", "datatype" : "_IRI"}]},
                            "datatype" : "_JSONMAP",
                            "annotation" : {},
                            "graph" : "graph",
@@ -266,9 +266,9 @@ fn intersection_expression() {
 #[test]
 fn one_of() {
     let one_of = r#" {"subject": "ex:oneOf",
-                      "predicate": "rdfs:subClassOf",
-                      "object": {"rdf:type": [{"object": "owl:Class", "datatype": "_IRI"}],
-                                 "owl:oneOf": [{"object" : [{"object" : "ex:a1", "datatype": "_IRI"},
+                      "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                      "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Class>", "datatype": "_IRI"}],
+                                 "<http://www.w3.org/2002/07/owl#oneOf>": [{"object" : [{"object" : "ex:a1", "datatype": "_IRI"},
                                                             {"object" : "ex:a2", "datatype": "_IRI"},
                                                             {"object" : "ex:a3", "datatype": "_IRI"}],
                                                "datatype": "_JSONLIST"}]},
@@ -286,9 +286,9 @@ fn one_of() {
 #[test]
 fn union_of() {
     let union_of = r#" {"subject": "ex:union",
-                        "predicate": "rdfs:subClassOf",
-                        "object": {"rdf:type": [{"object": "owl:Class", "datatype": "_IRI"}],
-                                   "owl:unionOf": [{"object" : [{"object" : "ex:u1", "datatype": "_IRI"},
+                        "predicate": "<http://www.w3.org/2000/01/rdf-schema#subClassOf>",
+                        "object": {"<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>": [{"object": "<http://www.w3.org/2002/07/owl#Class>", "datatype": "_IRI"}],
+                                   "<http://www.w3.org/2002/07/owl#unionOf>": [{"object" : [{"object" : "ex:u1", "datatype": "_IRI"},
                                                                 {"object" : "ex:u2", "datatype": "_IRI"},
                                                                 {"object" : "ex:u3", "datatype": "_IRI"}],
                                                     "datatype": "_JSONLIST"}]},
